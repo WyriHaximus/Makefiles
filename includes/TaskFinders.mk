48509e976d8a6dd53227fcd505b6b6452cae2ea1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-03-15 22:48:00 +0100

    Fix task finders

--- includes/TaskFinders.mk
+++ includes/TaskFinders.mk
@@ -1,3 +1,6 @@
+pljafojwkefsalm: ## Run static analysis (Psalm)
+	$(DOCKER_RUN) vendor/bin/psalm --threads=$(THREADS) --shepherd --stats --config=./etc/qa/psalm.xml
+
 task-list-ci: ## CI: Generate a JSON array of jobs to run, matches the commands run when running `make (|all)` ####
 	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sed 's/[^:]*://' | grep "##\%##" | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "%s\n", $$1}' | jq --raw-input --slurp -c 'split("\n")| .[0:-1]'
 
