8eb4a24ef64563290bfe64922a0cef41157510a3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-04-16 07:48:53 +0200

    Switch to generating makefiles for packages
    Turns out including them works but has some drawbacks. The biggest being they don't work on first install which includes CI.

--- includes/All.mk
+++ includes/All.mk
@@ -1,2 +1,2 @@
 all: ## Runs everything ####
-	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sed 's/[^:]*://' | grep -v "####" | awk 'BEGIN {FS = ":.*?## "}; {printf "%s\n", $$1}' | xargs --open-tty -I % bash -c 'cd $$PWD && $(MAKE) % || exit 255'
+	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | grep -v "####" | awk 'BEGIN {FS = ":.*?## "}; {printf "%s\n", $$1}' | xargs --open-tty $(MAKE)
