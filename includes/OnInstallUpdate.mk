a987dbddeefd9f1aee476f27975e43206d95aac1
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-09-04 21:37:52 +0200

    Run on install or update twice

--- includes/OnInstallUpdate.mk
+++ includes/OnInstallUpdate.mk
@@ -1,2 +1,3 @@
 on-install-or-update: ## Runs everything ####
 	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | grep -E "##\*(I|ILH)\*##" | awk 'BEGIN {FS = ":.*?## "}; {printf "%s\n", $$1}' | xargs -o $(MAKE)
+	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | grep -E "##\*(I|ILH)\*##" | awk 'BEGIN {FS = ":.*?## "}; {printf "%s\n", $$1}' | xargs -o $(MAKE)
