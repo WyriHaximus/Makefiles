edd4de16ef1381d7090ee026bda3372fa177b076
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-09-07 15:07:00 +0200

    Add PHP Migration: Fix logs paths in etc/qa/infection.json5

--- includes/PHP.Migrations.mk
+++ includes/PHP.Migrations.mk
@@ -3,3 +3,6 @@ php-migrations-move-infection: #### Move infection.json.dist to etc/qa/infection
 
 php-migrations-remove-phpunit-config-dir-from-infection: #### Drop XXX from etc/qa/infection.json5 ##*I*##
 	($(DOCKER_RUN) php -r '$$infectionFile = "etc/qa/infection.json5"; if (!file_exists($$infectionFile)) {exit;} $$json = json_decode(file_get_contents($$infectionFile), true); if (!is_array($$json)) {exit;}  if (!array_key_exists("phpUnit", $$json)) {exit;} unset($$json["phpUnit"]); file_put_contents($$infectionFile, json_encode($$json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\r\n");' || true)
+
+php-migrations-fix-logs-relative-paths-for-infection: #### Fix logs paths in etc/qa/infection.json5 ##*I*##
+	($(DOCKER_RUN) php -r '$$infectionFile = "etc/qa/infection.json5"; if (!file_exists($$infectionFile)) {exit;} $$json = json_decode(file_get_contents($$infectionFile), true); if (!is_array($$json)) {exit;}  if (!array_key_exists("logs", $$json)) {exit;} foreach ($$json["logs"] as $$logsKey => $$logsPath) { $$json["logs"][$$logsKey] = str_replace("\"./var/infection", "\"../../var/infection", $$json["logs"][$$logsKey]); } file_put_contents($$infectionFile, json_encode($$json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\r\n");' || true)
