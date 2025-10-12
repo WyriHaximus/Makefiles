17708e339eb892b9e9393b690114ee4e28b35d93
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-10-12 19:40:58 +0200

    Create Renovate Config if it doesn't exists at .github/renovate.json

--- includes/Renovate.Migrations.mk
+++ includes/Renovate.Migrations.mk
@@ -5,5 +5,8 @@ migrations-renovate-remove-dependabot-config: #### Make sure we remove .github/d
 migrations-renovate-move-config: #### Move renovate.json to .github/renovate.json ##*I*##
 	($(DOCKER_RUN) mv renovate.json .github/renovate.json || true)
 
+migrations-renovate-create-config-if-not-exists: #### Create Renovate Config if it doesn't exists at .github/renovate.json ##*I*##
+	($(DOCKER_RUN) php -r '$$renovateConfigFile = ".github/renovate.json"; $$renovateConfigContents = base64_decode("ewogICIkc2NoZW1hIjogImh0dHBzOi8vZG9jcy5yZW5vdmF0ZWJvdC5jb20vcmVub3ZhdGUtc2NoZW1hLmpzb24iLAogICJleHRlbmRzIjogWwogICAgImdpdGh1Yj5XeXJpSGF4aW11cy9yZW5vdmF0ZS1jb25maWc6cGhwLXBhY2thZ2UiCiAgXQp9Cg=="); if (file_exists($$renovateConfigFile)) {exit;} file_put_contents($$renovateConfigFile, $$renovateConfigContents);' || true)
+
 migrations-renovate-point-at-correct-config: #### Ensure .github/renovate.json points at github>WyriHaximus/renovate-config:php-package instead of local>WyriHaximus/renovate-config ##*I*##
 	($(DOCKER_RUN) php -r '$$renovateFIle = ".github/renovate.json"; if (!file_exists($$renovateFIle)) {exit;} file_put_contents($$renovateFIle, str_replace("local>WyriHaximus/renovate-config", "github>WyriHaximus/renovate-config:php-package", file_get_contents($$renovateFIle)));' || true)
