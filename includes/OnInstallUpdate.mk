bb6ab6d32b2418964d2017997d06c92cd09df80e
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-11-26 22:30:50 +0100

    Fix returning list of tasks instead of running them like an idiot

--- includes/OnInstallUpdate.mk
+++ includes/OnInstallUpdate.mk
@@ -1,2 +1,2 @@
 on-install-or-update: ## Tasks, like migrations, that specifically have be run after composer install or update. These will also run by self hosted Renovate ####
-	$(MAKE) make-list(on-install-or-update)
+	make-list(on-install-or-update)
