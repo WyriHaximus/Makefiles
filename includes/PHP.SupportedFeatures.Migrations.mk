833df92096a0f09b587b07a21585f68c1b4ff7a8
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2026-06-11 23:10:24 +0200

    Ensure we remove the PHPUnit config file when unit-tests aren't enabled

--- includes/PHP.SupportedFeatures.Migrations.mk
+++ includes/PHP.SupportedFeatures.Migrations.mk
@@ -3,3 +3,6 @@ migrations-supported-features-php-ensure-we-only-cs-check-and-fix-tests-if-unit-
 
 migrations-supported-features-php-ensure-we-only-staticly-analyse-tests-with-phpstan-if-unit-tests-is-enabled: #### Ensure we only staticly analyse tests/ with PHPStan if unit-tests is enabled ##*I*##
 	($(DOCKER_RUN) php -r 'if (in_array("unit-tests", supported-features(raw))) {exit;} $$phpStanCongifFIle = "etc/qa/phpstan.neon"; $$fileContents = explode("\n", file_get_contents($$phpStanCongifFIle)); foreach ($$fileContents as $$lineNumber => $$lineContent) { if (str_contains($$lineContent, "- ../../tests")) { unset($$fileContents[$$lineNumber]); } } file_put_contents($$phpStanCongifFIle, implode("\n", $$fileContents));' || true)
+
+migrations-supported-features-php-ensure-no-phpunit-config-file-is-present-when-unit-tests-are-disabled: #### Ensure we remove the PHPUnit config file when unit-tests aren't enabled ##*I*##
+	($(DOCKER_RUN) php -r 'if (in_array("unit-tests", supported-features(raw))) {exit;} @unlink("etc/qa/phpunit.xml");' || true)
